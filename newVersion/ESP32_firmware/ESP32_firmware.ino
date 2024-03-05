ae2a00d74fa17e62dc95d12e76d32e3ab1c710b5
Benno <81779508+Westpol@users.noreply.github.com>
2024-03-05 22:59:12 +0100

    setup standard variables

--- newVersion/ESP32_firmware/ESP32_firmware.ino
+++ newVersion/ESP32_firmware/ESP32_firmware.ino
@@ -1,11 +1,27 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
+#define CS    -1  // Declaration of Chip Select
+#define DC     4  // Declaration of Data / Command Pin
+#define RST    2   // Declaration of RESET Pin
+
+Adafruit_ST7789 display = Adafruit_ST7789(CS, DC, RST);
+
 
 void setup(){
   Serial2.begin(115200);
   Serial.begin(115200);
+
+  display.init(240, 240, SPI_MODE2);
+
 }
 
 void loop(){
   if (Serial2.available() > 0) {
     Serial.write(Serial2.read());
   }
+  display.fillScreen(ST77XX_BLACK);
+  display.setTextSize(2);
+  display.println("Hello World!");
 }
